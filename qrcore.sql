1c26c8e7eb635cec34e193b9fb51fd5a40f8ad38
R S K <66393045+mn9-29@users.noreply.github.com>
2022-11-23 10:36:25 +0300

    ..

--- qrcore.sql
+++ qrcore.sql
@@ -131,28 +131,26 @@ CREATE TABLE `player_contacts` (
   KEY `citizenid` (`citizenid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `player_vehicles` (
+CREATE TABLE IF NOT EXISTS `player_horses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `license` varchar(50) DEFAULT NULL,
   `citizenid` varchar(50) DEFAULT NULL,
-  `vehicle` varchar(50) DEFAULT NULL,
-  `hash` varchar(50) DEFAULT NULL,
-  `mods` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `plate` varchar(50) NOT NULL,
-  `fakeplate` varchar(50) DEFAULT NULL,
-  `garage` varchar(50) DEFAULT NULL,
-  `fuel` int(11) DEFAULT 100,
-  `engine` float DEFAULT 1000,
-  `body` float DEFAULT 1000,
-  `state` int(11) DEFAULT 1,
-  `depotprice` int(11) NOT NULL DEFAULT 0,
-  `drivingdistance` int(50) DEFAULT NULL,
-  `status` text DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `plate` (`plate`),
-  KEY `citizenid` (`citizenid`),
-  KEY `license` (`license`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `name` varchar(255) NOT NULL,
+  `horse` varchar(50) DEFAULT NULL,
+  `components` varchar(50) DEFAULT NULL,
+  `active` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `player_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `vehicles` varchar(50) DEFAULT NULL,
+  `components` varchar(50) DEFAULT NULL,
+  `active` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+
 
 CREATE TABLE `players` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
